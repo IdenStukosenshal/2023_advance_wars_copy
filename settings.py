13e77a15c32feb1f8d471755f1a2b85ef0a65e23
Siegfried_Zalissigun <Cilob_Bisolmafol@outlook.com>
2023-07-13 12:39:34 +0700

    Произошла оптимизация

--- settings.py
+++ settings.py
@@ -8,8 +8,8 @@ class Settings:
 
         self.max_value = 90
 
-        self.weights_track = {'#': 1.5, 'd': 1, 'f': 2, '@': self.max_value, 'v': self.max_value, 't': 1.125}
-        self.weights_inf = {'#': 1, 'd': 1, 'f': 1.3, '@': 2, 'v': 2, 't': 1}
+        self.weights_track = {'#': 1.5, 'd': 1, 'f': 2, '@': self.max_value, 'v': self.max_value, 't': 1.125, 'inaccessible': self.max_value}
+        self.weights_inf = {'#': 1, 'd': 1, 'f': 1.3, '@': 2, 'v': 2, 't': 1, 'inaccessible': self.max_value}
 
         self.recon_points = 6
 
